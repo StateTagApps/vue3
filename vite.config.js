38602d26e92e4162cc36f28182d3d221bd470ba1
Tarek Adam <tarek.adam@gmail.com>
2025-09-09 10:30:15 -0600

    build puts files in dist correctly

--- vite.config.js
+++ vite.config.js
@@ -1,67 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
-    publicDir: false,
-    root: '.',
-    plugins: [
-        vue(),
-
-        // Inject src/main.js in dev
-        {
-            name: 'sta-inject-entry-dev',
-            apply: 'serve',
-            transformIndexHtml() {
-                return [
-                    {
-                        tag: 'script',
-                        attrs: { type: 'module', src: '/@vite/client' },
-                        injectTo: 'head'
-                    },
-                    {
-                        tag: 'script',
-                        attrs: { type: 'module', src: '/src/main.js' },
-                        injectTo: 'head'
-                    }
-                ]
-            }
-        },
-
-        // Inject app.js in build
-        {
-            name: 'sta-inject-entry-build',
-            apply: 'build',
-            transformIndexHtml(html) {
-                if (html.includes('src="./app.js"')) return html
-                return html.replace(
-                    /<\/head>/i,
-                    '    <script src="./app.js"></script>\n  </head>'
-                )
-            }
-        }
-    ],
-
+    plugins: [vue()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@': resolve(__dirname, 'src')
         }
     },
-
-    server: {
-        open: '/index.html',
-        fs: { strict: false }
-    },
-
     build: {
         outDir: 'dist',
         emptyOutDir: true,
         rollupOptions: {
-            input: 'public/index.html',
+            input: resolve(__dirname, 'src/main.js'),
             output: {
-                entryFileNames: 'app.js',
-                chunkFileNames: 'app.js',
-                assetFileNames: '[name][extname]'
+                entryFileNames: 'app.js'
             }
         }
     }
